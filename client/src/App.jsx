c27e3d97199ba8aa5c7d3480fda9e9a918a6d726
sohel <sohelf131@gmail.com>
2025-02-21 01:22:36 +0600

    implemented google login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,29 @@ import { FcGoogle } from "react-icons/fc";
 import './App.css'
 import TaskForm from "./components/TaskForm/TaskForm";
 import Tasks from "./components/Tasks/Tasks";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "./provider/AuthProvider";
 
 function App() {
   const [newTask, setNewTask] = useState({})
+  const { user, googleSignIn, loading} = useContext(AuthContext)
 
   const setEditingTask = (task) => {
     setNewTask({...task, edit:true})
   }
-  const user = true;
+
+  const login = async() => {
+    const res = await googleSignIn();
+    console.log(res);
+  }
+
+  if(loading) return <h2>Loading....</h2>
+
   if(!user) {
     return(
       <div className='loginPage'>
             <h2>Login to Get Into!</h2>
-            <button> <FcGoogle /> Sign In With Google</button>
+            <button onClick={login}> <FcGoogle /> Sign In With Google</button>
       </div>
     )
   }
---
+++ client/src/provider/AuthProvider.jsx
@@ -0,0 +1,46 @@
+import { createContext, useState, useEffect } from "react";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null)
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider()
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);  
+            setLoading(false)     
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        googleSignIn,
+        logOut
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
